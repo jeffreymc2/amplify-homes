062078f86725a5fcdd2f9dc4611d5033172d7d9c
Jeff McDowell <jeffreymc@gmail.com>
2021-12-10 20:09:52 -0500

    master

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import { NavBar, MarketingFooter, CardBCollection, HeroLayout1 } from './ui-components'
 
 function App() {
   return (
     <div className="App">
+      <NavBar width={"100vw"}/>
+      <HeroLayout1/>
+      <CardBCollection isPaginated itemsPerPage={3}/>
+      <MarketingFooter width={"100vw"}/>
     </div>
   );
 }
---
+++ src/ui-components/CardBCollection.jsx
@@ -0,0 +1,47 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { Home } from "../models";
+import {
+  getOverrideProps,
+  useDataStoreBinding,
+} from "@aws-amplify/ui-react/internal";
+import CardB from "./CardB";
+import { Collection } from "@aws-amplify/ui-react";
+export default function CardBCollection(props) {
+  const { home, items: itemsProp, overrides: overridesProp, ...rest } = props;
+  const overrides = { ...overridesProp };
+  const items =
+    itemsProp !== undefined
+      ? itemsProp
+      : useDataStoreBinding({
+          type: "collection",
+          model: Home,
+        }).items;
+  return (
+    <Collection
+      templateColumns="1fr 1fr"
+      type="grid"
+      alignItems="stretch"
+      justifyContent="stretch"
+      autoFlow="row"
+      items={items || []}
+      {...rest}
+      {...getOverrideProps(overrides, "Collection")}
+    >
+      {(item, index) => (
+        <CardB
+          margin="0 10px 10px 0"
+          home={item}
+          key={item.id}
+          {...getOverrideProps(overrides, "Collection.CardB[0]")}
+        ></CardB>
+      )}
+    </Collection>
+  );
+}
